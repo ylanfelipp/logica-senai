559c806fe963efee7f8e792e027c63dd104e08f6
Ylan <94792864+ylanfelipp@users.noreply.github.com>
2024-05-08 22:11:18 -0300

    Delete exercicios-js directory

--- exercicios-js/for/ex04/script.js
+++
@@ -1,9 +0,0 @@
-let soma = 0
-
-let num = parseInt(prompt('Digite um número'))
-
-for (let i = 0; i <= num; i++) {
-    soma += i
-}
-
-alert(`Valor da soma: ${soma}`)
--- exercicios-js/while/ex01/script.js
+++
@@ -1,5 +0,0 @@
-let count = 1
-while(count <= 10) {
-    console.log(count)
-    count++
-}
