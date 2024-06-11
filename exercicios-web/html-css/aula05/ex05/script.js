c95fec18337ccf7c265743d67405157087d795e9
Ylan <ylanfelipp2@gmail.com>
2024-06-10 22:07:32 -0300

    Finalização da calculadora de IMC

--- exercicios-web/html-css/aula05/ex05/script.js
+++ exercicios-web/html-css/aula05/ex05/script.js
@@ -10,13 +10,17 @@ const img = document.querySelector('img')
 
 const imc = document.querySelector('.imc')
 
+const popup = document.querySelector('.wrapper')
+
+const inputs = document.querySelectorAll('input')
+const labels = document.querySelectorAll('label')
+const botao = document.querySelector('button')
+
 
 function avaliarUsuario(event) {
     event.preventDefault()
-
-    div.style.display = "block"
     const formData = new FormData(form)
-    
+
     const inputNome = formData.get('nome')
     const inputProfissao = formData.get('profissao')
     const inputPeso = formData.get('peso')
@@ -27,23 +31,55 @@ function avaliarUsuario(event) {
     nome.textContent = `Nome: ${inputNome}`
     profissao.textContent = `Profissão: ${inputProfissao}`
     peso.textContent = `Peso: ${inputPeso} Kg`
-    altura.textContent = `Altura: ${inputAltura}`
+    altura.textContent = `Altura: ${inputAltura}m`
 
-    let pesoFormatado = parseFloat(inputPeso.replace(',', '.'))
-    let alturaFormatada = parseFloat(inputAltura.replace(',', '.'))
-    let calculoIMC = pesoFormatado / (alturaFormatada * alturaFormatada)
+    let calculoIMC = inputPeso / (inputAltura * inputAltura)
 
-    console.log(pesoFormatado, alturaFormatada)
 
+    let classificacao
     if (calculoIMC < 18.5) {
-        imc.textContent = "Abaixo do Peso Ideal"
+        classificacao = "Status IMC: Abaixo do Peso Ideal"
     } else if (calculoIMC >= 18.5 && calculoIMC <= 24.9) {
-        imc.textContent = "Peso Ideal"
-    } else if (calculoIMC >=  25 && calculoIMC <= 29.9) {
-        imc.textContent = "Sobrepeso"
+        classificacao = "Status IMC: Peso Ideal"
+    } else if (calculoIMC >= 25 && calculoIMC <= 29.9) {
+        classificacao = "Sobrepeso"
     } else if (calculoIMC >= 30 && calculoIMC <= 39.9) {
-        imc.textContent = "Obesidade"
+        classificacao = "Status IMC: Obesidade"
     } else if (calculoIMC >= 40) {
-        imc.textContent = "Obesidade Grave"
+        classificacao = "Status IMC: Obesidade Grave"
     }
+
+    imc.textContent = classificacao
+
+    popup.style.display = "flex"
+
+    inputs.forEach((el) => {
+        el.style.display = "none"
+    })
+
+    labels.forEach(el => {
+        el.style.display = "none"
+    })
+
+    botao.style.display = "none"
 }
+
+
+popup.addEventListener('click', e => {
+    if (e.target !== popup) {
+        return
+    }
+
+    popup.style.display = "none"
+
+    inputs.forEach((el) => {
+        el.style.display = "inline"
+    })
+
+    labels.forEach(el => {
+        el.style.display = "inline"
+
+    })
+
+    botao.style.display = "inline"
+})
