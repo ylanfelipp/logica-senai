055b69826d4dd1372bae4bbc94c59ac8f544c922
Ylan <ylanfelipp2@gmail.com>
2024-06-24 21:56:36 -0300

    Finalização do conteúdo localStorage

--- exercicios-web/html-css/aula08/ex03/script.js
+++ exercicios-web/html-css/aula08/ex03/script.js
@@ -1,29 +1,42 @@
 let listaDeNomes = []
 
+const inputNome = document.querySelector('#id_nome') 
+inputNome.focus()
+
 function adicionarNomeNaLista(event) {
-    debugger
     event.preventDefault()
 
-    const inputNome = document.querySelector('#id_nome').value
-
-    
-    if (inputNome !== "" && inputNome  !== " ") {
-        listaDeNomes.push(inputNome)
+    if (inputNome.value !== "" && inputNome.value !== " ") {
+        listaDeNomes.push(inputNome.value.trim())
     }
 
     localStorage.setItem('lista', JSON.stringify(listaDeNomes))
+    location.reload() 
+}
+
+const lista = document.querySelector('tbody')
+
+const listaNomeLocalStorage = localStorage.getItem('lista')
+if (listaNomeLocalStorage) {
+    listaDeNomes = JSON.parse(
+        listaNomeLocalStorage
+    )
+}
+
+listaDeNomes.forEach(function(nome, index) {
+    lista.innerHTML += `
+            <tr>
+                <td class="index">${index + 1}</td>
+                <td>${nome}</td>
+                <td class="delete" onclick="deletarNome(${index})"><i class=" fa-regular fa-trash-can"></i></td>
+            </tr>
+        `
+})
+
+function deletarNome(indexNome) {
+    listaDeNomes = listaDeNomes.filter((nome, index) => {
+        return index !== indexNome
+    })
+    localStorage.setItem('lista', JSON.stringify(listaDeNomes))
     location.reload()
 }
-
-
-const lista = document.querySelector('.lista-de-nomes')
-
-const listaNomeLocalStorge = localStorage.getItem('lista')
-
-if (listaNomeLocalStorge) {
-    listaDeNomes = JSON.parse(localStorage.getItem('lista'))
-}
-
-listaDeNomes?.forEach(nome => {
-    lista.innerHTML += `<li>${nome}</li>`
-})
